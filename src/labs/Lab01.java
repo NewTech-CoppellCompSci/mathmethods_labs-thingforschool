38f719d6c70258befb0535d46878bef4d09a9808
thingforschool <rgirish@newtech.coppellisd.com>
2022-03-11 23:28:33 -0600

    aaa

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -15,43 +15,59 @@ public class Lab01 {
 	
 
 	
-	public static void problem01() {
+public static void problem01() {
 		
-		
-		
-	}
-	
-	
-	
-	
-	public static void problem02() {
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("Choose a positive integer");
+		int userNum2 = inKey.nextInt();
+		System.out.println(userNum + "^" + userNum2 + " " + Math.pow(userNum, userNum2));
 		
 		
 	}
-
-	
-	
-
-	public static void problem03() {
+public static void problem02() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("The square root of " + userNum + " is " + Math.sqrt(userNum));
 		
-		
+	}
+public static void problem03() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("What is the side A of a right triangle?");
+		int userNum = inKey.nextInt();
+		System.out.println("What is the side B of a right triangle?");
+		int userNum2 = inKey.nextInt();
+		System.out.println("Hyptoneuse = " + Math.sqrt(userNum)+(Math.pow(userNum2, userNum2)));
 		
 
 	}
-
-
-	
-	
-	public static void problem04() {
-		
-		
+public static void problem04() {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		Scanner inKey1 = new Scanner(System.in);
+		int userNum = 1;
+		boolean not0 = true;
+		while (not0) {
+			System.out.println("Choose an integer, choose 0 when you're done");
+			userNum = inKey1.nextInt();
+			if (userNum ==0) {
+				not0=false;
+			}
+			else {
+				if (userNum > max) {
+				}
+				else if (userNum < min) {
+					min = userNum;
+				}
+			}
+		}
+		System.out.println(max);
+		System.out.println(min + "\n");
 		
 	}
-	
-	
-	
-	
-	
-	
 }
+
+
+
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -16,41 +16,56 @@ public class Lab02 {
 
 	
 	public static void problem01() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("Choose a positive integer");
+		int userNum2 = inKey.nextInt();
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int x1=userNum;
+		int x2=userNum2;
 		
 		
+	}
+public static void problem02() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose radius of cylinder");
+		int radius = inKey.nextInt();
+		System.out.println("Choose height of cylinder");
+		int height = inKey.nextInt();
+		System.out.println(Math.pow(radius, 2) * height * Math.PI);
+		
+	}
+public static void problem03() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("enter x1");
+		double x1 = inKey.nextDouble();
+		System.out.println("enter y1");
+		double y1 = inKey.nextDouble();
+		System.out.println("enter x2");
+		double x2 = inKey.nextDouble();
+		System.out.println("enter y2");
+		double y2 = inKey.nextDouble();
+		System.out.println( Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1 - y2, 2)));
+		
+		
+	}
+public static void problem04() {
+		Scanner 
+		
+		inKey = new Scanner(System.in);
+		System.out.print("enter a: ");
+		double a = inKey.nextDouble();
+		System.out.print("enter b: ");
+		double b = inKey.nextDouble();
+		System.out.print("enter c: ");
+		double c = inKey.nextDouble();
+		
+		System.out.println("x1 = " + ((-1*b + Math.sqrt(Math.pow(b, 2) -4*a*c)) / (2*a) ));
 		
 		
 	}
 	
-	
-	
-	
-	public static void problem02() {
-		
-		
-		
-	}
 
-	
-	
-
-	public static void problem03() {
-
-		
-		
-	}
-
-
-	
-	
-	public static void problem04() {
-		
-		
-		
-		
-	}
-	
-	
-
-	
 }
